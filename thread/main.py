b2b52d0c5f28854276e6720dccd861c5bbf9345b
kwakmu18 <mckkk119@naver.com>
2024-03-26 01:59:36 +0900

    스크롤바 수정
    20240326

--- thread/main.py
+++ thread/main.py
@@ -36,12 +36,15 @@ class TkinterUI:
         self.frame3 = tk.Frame(self.window) # log
         self.frame3.place(x=0, y=300, width=400, height=780)
 
+        self.lbox = tk.Listbox(self.frame3, width=200, height=400)
+        self.lbox.place(x=10, y=0, width=370, height=680)
         self.vscroll = tk.Scrollbar(self.frame3, orient="vertical")
         self.hscroll = tk.Scrollbar(self.frame3, orient="horizontal")
-        self.lbox = tk.Listbox(self.frame3, xscrollcommand=self.hscroll.set, yscrollcommand=self.vscroll.set, width=200, height=500)
         self.vscroll.config(command=self.lbox.yview)
         self.hscroll.config(command=self.lbox.xview)
-        self.lbox.pack(); self.vscroll.pack(side="right", fill="y"); self.hscroll.pack()
+        self.vscroll.place(x=380, y=0, width=20, height=680)
+        self.hscroll.place(x=10,y=680, width=370, height=20)
+        self.lbox.config(xscrollcommand=self.hscroll.set, yscrollcommand=self.vscroll.set)
 
         self.modeRadio1 = tk.Radiobutton(self.frame1, text="root node", font=self.font, variable=self.mode, value=0, command=self.radioButtonPressed)
         self.modeRadio1.place(x=50, y=275)
@@ -149,48 +152,57 @@ class TkinterUI:
         self.startButton["state"] = "disabled"
     def binaryUpload(self):
         top = tk.Toplevel(self.window)
+        top.attributes("-topmost", 1)
         top.geometry("400x600")
         top.title("Binary Upload")
         self.contiki_path, self.node_cnt, self.actuator_id, self.node_id, self.node_type, self.data_cnt = \
             (tk.StringVar() for _ in range(6))
         contiki_path_label = tk.Label(top, text="Contiki-NG Path : ", font=self.font)
-        contiki_path_input = tk.Entry(top, textvariable = self.contiki_path)
+        self.contiki_path_input = tk.Entry(top, textvariable = self.contiki_path)
         node_cnt_label = tk.Label(top, text="Node Cnt : ", font=self.font)
-        node_cnt_input = tk.Entry(top, textvariable=self.node_cnt)
+        self.node_cnt_input = tk.Entry(top, textvariable=self.node_cnt)
         actuator_id_label = tk.Label(top, text="Actuator ID : ", font=self.font)
-        actuator_id_input = tk.Entry(top, textvariable=self.actuator_id)
+        self.actuator_id_input = tk.Entry(top, textvariable=self.actuator_id)
         node_id_label = tk.Label(top, text="Node ID : ", font=self.font)
         node_id_input = tk.Entry(top, textvariable=self.node_id)
         node_type_label = tk.Label(top, text="Node Type : ", font=self.font)
         node_type_input = ttk.Combobox(top, values=self.ld.NODE_TYPE[1:], textvariable=self.node_type)
         data_cnt_label = tk.Label(top, text="Data Cnt : ", font=self.font)
-        data_cnt_input = tk.Entry(top, textvariable=self.data_cnt)
+        self.data_cnt_input = tk.Entry(top, textvariable=self.data_cnt)
 
-        contiki_path_input.insert(0, "/home/mckkk119/CNLAB_EV/contiki-ng")
-        node_cnt_input.insert(0, "4")
-        actuator_id_input.insert(0, "3")
+        self.contiki_path_input.insert(0, "/home/mckkk119/CNLAB_EV/contiki-ng")
+        self.node_cnt_input.insert(0, "4")
+        self.actuator_id_input.insert(0, "3")
         node_id_input.insert(0, "1")
-        data_cnt_input.insert(0, "100")
+        self.data_cnt_input.insert(0, "100")
         node_type_input.insert(0, "AP")
 
         contiki_path_label.place(x=10, y=10)
-        contiki_path_input.place(x=120, y=10, width=260, height=25)
+        self.contiki_path_input.place(x=120, y=10, width=260, height=25)
         node_cnt_label.place(x=10, y=45)
-        node_cnt_input.place(x=120, y=45, width=260, height=25)
+        self.node_cnt_input.place(x=120, y=45, width=260, height=25)
         actuator_id_label.place(x=10, y=80)
-        actuator_id_input.place(x=120, y=80, width=260, height=25)
+        self.actuator_id_input.place(x=120, y=80, width=260, height=25)
         node_id_label.place(x=10, y=115)
         node_id_input.place(x=120, y=115, width=260, height=25)
         node_type_label.place(x=10, y=150)
         node_type_input.place(x=120, y=150, width=260, height=25)
         data_cnt_label.place(x=10, y=185)
-        data_cnt_input.place(x=120, y=185, width=260, height=25)
+        self.data_cnt_input.place(x=120, y=185, width=260, height=25)
 
         uploadButton = tk.Button(top, text="Upload to node", command=self.upload, font=self.font)
         uploadButton.place(x=10, y=220, width=380, height=70)
 
         self.uploadLog = tk.Listbox(top)
-        self.uploadLog.place(x=10, y=300, width=380, height=280)
+        self.uploadLog.place(x=10, y=300, width=360, height=260)
+
+        vscroll = tk.Scrollbar(top, orient="vertical")
+        hscroll = tk.Scrollbar(top, orient="horizontal")
+        vscroll.config(command=self.uploadLog.yview)
+        hscroll.config(command=self.uploadLog.xview)
+        vscroll.place(x=370, y=300, width=20, height=260)
+        hscroll.place(x=10,y=560, width=360, height=20)
+        self.uploadLog.config(xscrollcommand=hscroll.set, yscrollcommand=vscroll.set)
 
     def upload(self):
         if not DEBUG_MODE:
@@ -203,6 +215,12 @@ class TkinterUI:
             or self.node_id.get()=="" or self.node_type.get()=="" or self.data_cnt.get()=="":
             msgbox.showwarning("ERROR", "Make sure all boxes are filled.")
             return
+        if self.node_id.get() == self.actuator_id.get() and self.node_type.get() != "ACTUATOR":
+            msgbox.showwarning("ERROR", f"Node {self.node_id.get()}'s type must be ACTUATOR.")
+            return
+        if self.node_id.get() > self.node_cnt.get():
+             msgbox.showwarning("ERROR", f"Node ID {self.node_id.get()} is bigger than node cnt {self.node_cnt.get()}.")
+             return
         try:
             content = ""
             with open(self.contiki_path.get()+"/os/contiki-main.c", "r") as f:
@@ -267,6 +285,10 @@ class TkinterUI:
                 line = proc.stdout.readline()
                 if not line:break
                 self.appendLog(line)
+        self.node_cnt_input["state"] = "disabled"
+        self.contiki_path_input["state"] = "disabled"
+        self.data_cnt_input["state"] = "disabled"
+        self.actuator_id_input["state"] = "disabled"
 
     def appendLog(self, line):
         self.uploadLog.insert(tk.END, line)
